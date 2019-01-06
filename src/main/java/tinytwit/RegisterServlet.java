d272ca0c447505b02cc42abfe54dede3ce06f81b
aliegeois <pelanman@gmail.com>
2019-01-06 15:46:49 +0100

    Ctrl + shift + O

--- src/main/java/tinytwit/RegisterServlet.java
+++ src/main/java/tinytwit/RegisterServlet.java
@@ -3,17 +3,12 @@ package tinytwit;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 
 public class RegisterServlet extends HttpServlet {
